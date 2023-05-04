9ff98889d110cb4ebf1f2370bc00ace6aeade5cc
CahenTaylor <cahentaylor@gmail.com>
2023-05-03 20:23:50 -0600

    timed out

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+def countdown(num):
+    while num >= 1:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    else:
+        print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    while num >= 1:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+        time.sleep(1)
+    else:
+        print("HAPPY NEW YEAR!")
